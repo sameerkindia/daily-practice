4b5ec1ee99a5306ef0abb822a42340bba849ff95
sameer khan <sameerkhanaa5@gmail.com>
2023-12-21 20:56:21 +0530

    first commit

---
+++ day7.js
@@ -0,0 +1,114 @@
+const obj = { two: 2 };
+
+const { one, two } = obj;
+// console.log(one);
+// console.log(two);
+
+function moodToday(str) {
+  const result = `Today, I am feeling ${str || "neutral"}`;
+  return result;
+}
+
+// console.log(moodToday("happy"));
+// console.log(moodToday("sad"));
+// console.log(moodToday());
+
+function hasSameBread(sandwich1, sandwich2) {
+  const result =
+    sandwich1[0] === sandwich1[2] &&
+    sandwich2[0] === sandwich2[2] &&
+    sandwich1[0] === sandwich2[2];
+
+  return result;
+}
+
+// console.log(
+//   hasSameBread(
+//     ["white bread", "lettuce", "white bread"],
+//     ["white bread", "tomato", "white bread"]
+//   )
+// );
+// console.log(
+//   hasSameBread(
+//     ["brown bread", "chicken", "brown bread"],
+//     ["white bread", "chicken", "white bread"]
+//   )
+// );
+// console.log(
+//   hasSameBread(["toast", "cheese", "toast"], ["brown bread", "cheese", "toast"])
+// );
+
+function nSidedShape(n) {
+  switch (n) {
+    case 1:
+      return "circle";
+    case 2:
+      return "semi-circle";
+    case 3:
+      return "triangle";
+    case 4:
+      return "square";
+    case 5:
+      return "pentagon";
+    case 6:
+      return "hexagon";
+    case 7:
+      return "heptagon";
+    case 8:
+      return "octagon";
+    case 9:
+      return "nonagon";
+    case 10:
+      return "decagon";
+
+    default:
+      return "invaild input";
+  }
+}
+
+// console.log(nSidedShape(3));
+// console.log(nSidedShape(1));
+// console.log(nSidedShape(9));
+
+function getMultipliedArr(arr) {
+  const result = arr.map((el) => el * 2);
+  return result;
+}
+
+// console.log(getMultipliedArr([2, 5, 3]));
+// console.log(getMultipliedArr([1, 86, -5]));
+// console.log(getMultipliedArr([5, 382, 0]));
+
+function longBurp(n = 1) {
+  let str = "Bu";
+  for (let i = 0; i < n; i++) {
+    if (i === n - 1) {
+      return (str += "rp");
+    } else {
+      str += "r";
+    }
+  }
+}
+
+// console.log(longBurp(3));
+// console.log(longBurp(5));
+// console.log(longBurp(9));
+
+const obj1 = {
+  name: "John",
+  email: "john@example.com",
+  city: "Phoenix",
+  state: "AZ",
+  country: "USA",
+};
+
+let { name, email, ...rest } = obj1;
+
+// console.log(rest);
+
+function ctoa(char) {
+  const result = String.charCodeAt(char);
+  return result;
+}
+
+console.log(ctoa("A"));
